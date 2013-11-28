48f4b237c650d48265e6e5dd321b118a11735e9d
Michele Orsi <morsi@map2app.com>
2013-11-28 23:21:25 +0100

    added comments on linking lib

--- src/main/java/it/micheleorsi/endpoints/MessageResource.java
+++ src/main/java/it/micheleorsi/endpoints/MessageResource.java
@@ -10,6 +10,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -17,29 +18,32 @@ import javax.ws.rs.core.MediaType;
  * @author micheleorsi
  *
  */
-@Path("/messages")
+@Path("messages")
 public class MessageResource {
 	
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public Message getResource() {
-		return new Message("text subject","text body");
+	@Path("{id}")
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public Message getResource(@PathParam("id") String id) {
+		return new Message("text subject","text body",id);
 	}
 	
 	@POST
-	@Produces(MediaType.APPLICATION_JSON)
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 	public Message createResource(Message message) {
 		return message;
 	}
 	
 	@PUT
-	@Produces(MediaType.APPLICATION_JSON)
+	@Path("{id}")
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 	public Message updateResource(Message message) {
 		return message;
 	}
 	
 	@DELETE
-	@Produces(MediaType.APPLICATION_JSON)
-	public void deleteResource() {}
+	@Path("{id}")
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public void deleteResource(@PathParam("id") String id) {}
 
 }
--- src/main/java/it/micheleorsi/model/Message.java
+++ src/main/java/it/micheleorsi/model/Message.java
@@ -11,20 +11,28 @@ import javax.xml.bind.annotation.XmlRootElement;
  */
 @XmlRootElement
 public class Message {
+//	@Ref(resource=MessageResource.class)
+//	URI u;
 	
 	private String subject = null;
 	private String body = null;
+	private String id = null;
 	
 	public Message() {}
 	
-	public Message(String subject, String body) {
+	public Message(String subject, String body, String id) {
 		this.subject = subject;
 		this.body = body;
+		this.id = id;
 	}
 	
 	public String getSubject() {return subject;}
 	public void setSubject(String subject) {this.subject = subject;}
+	
 	public String getBody() {return body;}
 	public void setBody(String body) {this.body = body;}
+
+	public String getId() {return id;}
+	public void setId(String id) {this.id = id;}
 
 }

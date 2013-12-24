298bb5bd9b92def8dc9a37fabf2926c4ea9629ba
Michele Orsi <morsi@map2app.com>
2013-12-24 01:48:19 +0100

    added resource filter factory

--- src/main/java/it/micheleorsi/web/MainGuice.java
+++ src/main/java/it/micheleorsi/web/MainGuice.java
@@ -9,6 +9,7 @@ import java.util.logging.Logger;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.Singleton;
 import com.google.inject.servlet.GuiceServletContextListener;
 import com.sun.jersey.guice.JerseyServletModule;
 import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
@@ -21,15 +22,35 @@ public class MainGuice extends GuiceServletContextListener {
     // (1) Override getInjector
     @Override
     protected Injector getInjector() {
+    	
+    	final Map<String, String> params = new HashMap<String, String>();
+        /*
+         * The following line will scan ausbdsoccer.server.resources package for Jersey Resources
+         */
+        params.put("com.sun.jersey.config.property.packages","it.micheleorsi.endpoints");
+         
+        params.put("com.sun.jersey.api.json.POJOMappingFeature", "true");
+        
+        params.put("com.sun.jersey.config.feature.DisableWADL", "true");
+        
+        params.put("com.sun.jersey.spi.container.ResourceFilters", "it.micheleorsi.auth.filter.ResourceFilterFactory");
+        
+        
+        /*
+         * The following line will use MainJerseyApplication.java to define Jersey resources
+         */
+//        params.put("javax.ws.rs.Application", "it.micheleorsi.web.MainJerseyApplication");
+    	
         return Guice.createInjector(new JerseyServletModule() {
             @Override
             protected void configureServlets() {
 //                bind(SecurityFilter.class).in(Singleton.class);
+//            	bind(MainJerseyApplication.class).in(Singleton.class);
                 bind(MessageResource.class).to(MessageResourceImpl.class);// .in(Singleton.class);
 //                bind(ServletContainer.class).in(Singleton.class);
 
                 // (2) Change to using the GuiceContainer
-                serve("/rest/*").with(GuiceContainer.class); // <<<<---
+                serve("/rest/*").with(GuiceContainer.class,params); // <<<<---
 
 //                bind(UserDao.class).to(UserJdbc.class);
 //                bind(SessionUtility.class);
